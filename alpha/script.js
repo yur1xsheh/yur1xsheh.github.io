5473951826519ca100e9471939b5ac1a664f08ce
Hiepnguyn.moonlight <sta8494@gmail.com>
2023-09-21 00:16:33 +0700

    anh con 5phut

--- alpha/script.js
+++ alpha/script.js
@@ -1,19 +1,40 @@
-const player = document.getElementById("player");
-const playPause = document.getElementById("play-pause");
-const stop = document.getElementById("stop");
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
 
-playPause.addEventListener("click", function() {
-  if (player.paused) {
-    player.play();
-    playPause.innerHTML = "pause.";
-  } else {
-    player.pause();
-    playPause.innerHTML = "play.";
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      nextYear = yyyy + 1,
+      dayMonth = "11/01/",
+      birthday = dayMonth + yyyy;
+  
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = dayMonth + nextYear;
   }
-});
+  //end
+  
+  const countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
 
-stop.addEventListener("click", function() {
-  player.pause();
-  player.currentTime = 0;
-  playPause.innerHTML = "play.";
-});
+        const now = new Date().getTime(),
+              distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        if (distance < 0) {
+          document.getElementById("headline").innerText = "It's my birthday!";
+          document.getElementById("countdown").style.display = "none";
+          document.getElementById("content").style.display = "block";
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
